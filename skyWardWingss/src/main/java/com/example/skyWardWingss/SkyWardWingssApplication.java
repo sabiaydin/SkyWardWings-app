83ff44852b4f575f8aa93321c4a0e80e1822109a
sabiaydin <sabinamammadova871@gmail.com>
2025-02-16 18:15:29 +0400

    some changes

--- skyWardWingss/src/main/java/com/example/skyWardWingss/SkyWardWingssApplication.java
+++ skyWardWingss/src/main/java/com/example/skyWardWingss/SkyWardWingssApplication.java
@@ -4,19 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class SkyWardWingssApplication/* implements CommandLineRunner*/ {
-//	private final BCryptPasswordEncoder passwordEncoder;
-
+public class SkyWardWingssApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SkyWardWingssApplication.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println(passwordEncoder.encode("sabina"));
-//	}
 }
